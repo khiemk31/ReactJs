87cd4063cef88dd00e88428db78aad259800b856
khiemk31 <khiemnd@d2t.vn>
2022-06-13 14:46:56 +0700

    two_way_binding

--- fake_tiktok/src/App.js
+++ fake_tiktok/src/App.js
@@ -1,42 +1,90 @@
 import { useState } from "react";
 
-//Hooks : những metol , function đc viết sẵn sử dụng trong reactJS k sd trong class Component
-//useState hỗ trợ thay đổi dữ liệu (dữ liệu thay đổi giao diện thay đổi theo)
-const orders = [100, 200, 300, 500];
+const listGift = ["gift1", "gift2", "gift3"];
+
+const courses = [
+  {
+    id: 1,
+    name: "Course 1",
+  },
+  {
+    id: 2,
+    name: "Course 2",
+  },
+  {
+    id: 3,
+    name: "Course 3",
+  },
+];
 function App() {
-  //k code ntn vì mỗi lần gọi lại setCounter đều thực hiện tính lại total :
-  // const total = orders.reduce((total, current) => total + current);
-  // console.log(total);
-  // const [counter, setCounter] = useState(total);
-  //Cách đúng (giúp tối ưu Performance ) :
-  // const [counter, setCounter] = useState(() => {
-  //   const total = orders.reduce((total, current) => total + current);
-  //   console.log(total);
-  //   return total;
-  // });
-  // const handleClick = () => {
-  // setCounter(counter + 1);
-  // setCounter((prevState) => prevState + 1);
-  //+1 prevState= counter+1
-  // setCounter((prevState) => prevState + 1);
-  //+ thêm 1 prevState= counter+1+1
-  //prevState giá trị trước đó của state
-  // };
-  const [info, setInfo] = useState({
-    name: "Nguyen Duy Khiem",
-    age: 24,
-    address: "Ha Noi ",
-  });
-  const handleUpdateInfo = () => {
-    setInfo({
-      ...info,
-      bio: "asdfgdsf",
+  const [checked, setChecked] = useState(2);
+
+  console.log(checked);
+  const handleSubmit = () => {
+    console.log({ id: checked });
+    console.log({ ids: checkbox });
+  };
+  const [checkbox, setCheckbox] = useState([]);
+  const handleCheckBoxChange = (id) => {
+    setCheckbox((check) => {
+      const isChecked = checkbox.includes(id);
+      if (isChecked) {
+        return checkbox.filter((item) => item !== id);
+      } else {
+        return [...check, id];
+      }
     });
   };
+  console.log({ id: checkbox });
+  const [gift, setGift] = useState();
+
+  // const getGiftButton = () => {
+  //   const index = Math.floor(Math.random() * listGift.length);
+  //   console.log(listGift[index]);
+  //   setGift((gift) => listGift[index]);
+  // };
+
+  const [name, setName] = useState();
   return (
-    <div className="App">
-      <h1>{JSON.stringify(info)}</h1>
-      <button onClick={handleUpdateInfo}>Click</button>
+    // <div className="App">
+    //   <h1>{gift || "Chưa có phần thưởng"}</h1>
+    //   <button onClick={getGiftButton}>Lấy phần thưởng</button>
+    // </div>
+    //One-way binding  : ràng buộc 1 chiều vì giao diện thay đổi thì state thay đổi VD:
+    // <div style={{ padding: 30 }}>
+    //   <input onChange={(text) => setName(text.target.value)} />
+    // </div>
+    //Tow-way binding : ràng buộc 2 chiều giao diện thay đổi state thay đổi và khi set value state lại thẻ input thì giao diện thay đổi VD:
+    // <div style={{ padding: 30 }}>
+    //   <input value={name} onChange={(text) => setName(text.target.value)} />
+    //   <button onClick={() => setName("Đã đc thay đổi giá trị")}>
+    //     Thay đổi dữ liệu Input
+    //   </button>
+    // </div>
+    <div style={{ padding: 30 }}>
+      <p>Radio</p>
+      {courses.map((course) => (
+        <div key={course.id}>
+          <input
+            type="radio"
+            checked={checked === course.id}
+            onChange={() => setChecked(course.id)}
+          />
+          {course.name}
+        </div>
+      ))}
+      <p>CheckBox</p>
+      {courses.map((course) => (
+        <div key={course.id}>
+          <input
+            type="checkbox"
+            checked={checkbox.includes(course.id)}
+            onChange={() => handleCheckBoxChange(course.id)}
+          />
+          {course.name}
+        </div>
+      ))}
+      <button onClick={handleSubmit}>Đăng ký</button>
     </div>
   );
 }
