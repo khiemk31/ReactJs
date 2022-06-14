dd5b19676b8de28cfd7c2b4bf4335f28d6dd6787
khiemk31 <khiemnd@d2t.vn>
2022-06-14 10:58:43 +0700

    todoListwith useState

--- fake_tiktok/src/App2.js
+++ fake_tiktok/src/App2.js
@@ -1,28 +1,77 @@
 import { useState } from "react";
 
 function App2() {
-  const storageJobs = JSON.parse(localStorage.getItem("jobs"));
-
   const [job, setJob] = useState("");
-  const [jobs, setJobs] = useState([]);
-  const handleSubmit = () => {
+  const [jobs, setJobs] = useState(() => {
+    const storageJobs = JSON.parse(localStorage.getItem("jobs"));
+    return storageJobs ?? [];
+  });
+  const handleAdd = () => {
     setJobs((prev) => {
       const newJobs = [...prev, job];
-      const jsonJobs = JSON.parse(JSON.stringify(newJobs));
+
+      //Save to local storage
+      const jsonJobs = JSON.stringify(newJobs);
       localStorage.setItem("jobs", jsonJobs);
       return newJobs;
     });
     setJob("");
   };
+  const handleDelete = (index) => {
+    console.log(index);
+    const newJobs = [...jobs];
+    newJobs.splice(index, 1);
+    setJobs(newJobs);
+    const jsonJobs = JSON.stringify(newJobs);
+    localStorage.setItem("jobs", jsonJobs);
+    return newJobs;
+  };
+
+  const handleDeleteAll = () => {
+    setJobs([]);
+    localStorage.removeItem("jobs");
+  };
   return (
     <div style={{ padding: 32 }}>
       <input value={job} onChange={(text) => setJob(text.target.value)} />
-      <button onClick={handleSubmit}>Add</button>
-      <ul>
-        {jobs.map((job, index) => (
-          <li key={index}>{job}</li>
-        ))}
-      </ul>
+      <button
+        onClick={handleAdd}
+        style={{
+          marginTop: 18,
+          fontSize: 15,
+          color: "#343122",
+          fontWeight: "bold",
+        }}
+      >
+        Thêm
+      </button>
+      {jobs.map((job, index) => (
+        <div style={{ padding: 10, width: 300 }} key={index}>
+          <p style={{ width: 200, float: "left", marginRight: 50 }}>{job}</p>
+          <button
+            onClick={() => handleDelete(index)}
+            style={{
+              marginTop: 18,
+              fontSize: 15,
+              color: "#343122",
+              fontWeight: "bold",
+            }}
+          >
+            Xóa
+          </button>
+        </div>
+      ))}
+      <button
+        onClick={handleDeleteAll}
+        style={{
+          marginTop: 20,
+          fontSize: 20,
+          color: "#343122",
+          fontWeight: "bold",
+        }}
+      >
+        Xóa hết
+      </button>
     </div>
   );
 }
