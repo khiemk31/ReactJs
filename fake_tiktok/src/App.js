2edabf15eae68e3a61599231a8d4c375ce94ac14
khiemk31 <khiemnd@d2t.vn>
2022-06-15 13:50:02 +0700

    no message

--- fake_tiktok/src/App.js
+++ fake_tiktok/src/App.js
@@ -1,4 +1,4 @@
-import { useState, memo } from "react";
+import { useState, useCallback } from "react";
 import Content from "./Content";
 //1. memo() -> Higher Order Component (HOC)
 //2. useCallback()
@@ -10,20 +10,15 @@ import Content from "./Content";
 function App() {
   const [show, setShow] = useState(true);
   const [count, setCount] = useState(0);
-  const [number, setNumber] = useState(0);
 
-  const changeNumberInContent = () => {
-    setNumber(Math.random());
-  };
-  const handleClick = () => {
-    setCount(count + 1);
-  };
+  const handleClick = useCallback(() => {
+    setCount((prev) => prev + 1);
+  }, []);
   return (
     <div style={{ padding: "10px  32px" }}>
       <button onClick={() => setShow(!show)}>{show ? "Hide" : "Show"}</button>
-      {show && <Content number={number} />}
+      {show && <Content onIncrease={handleClick} />}
       <h1>{count}</h1>
-      <button onClick={changeNumberInContent}>Change number in content</button>
       <button onClick={handleClick}>Click me!</button>
     </div>
   );
--- fake_tiktok/src/Content.js
+++ fake_tiktok/src/Content.js
@@ -1,10 +1,11 @@
 import { useRef, useState, memo } from "react";
 
-function Content({ number }) {
+function Content({ onIncrease }) {
   console.log("re-render");
   return (
     <div style={{ padding: "10px  32px" }}>
-      <h1>Hello World {number}</h1>
+      <h1>Hello World </h1>
+      <button onClick={onIncrease}>Click me!</button>
     </div>
   );
 }
