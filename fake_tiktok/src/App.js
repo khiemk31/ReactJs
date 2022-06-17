6608e6eacb54750516543001a77ae50880f7f5f6
khiemk31 <khiemnd@d2t.vn>
2022-06-17 10:12:11 +0700

    no message

--- fake_tiktok/src/App.js
+++ fake_tiktok/src/App.js
@@ -1,48 +1,20 @@
-import { useState, useReducer } from "react";
+import Content from "./Content";
+import "./App.css";
+import { useContext } from "react";
+import { ThemeContext } from "./ThemeContext";
+//Context Context
+//CompA => CompB => CompC
 
-//useState:
-//1. Init state : 0
-//2. Actions : Up (state+1) / Down (state-1)
+//1. Create context
+//2. Provider
+//3. Consumer
 
-//useReducer:
-//1. Init state : 0
-//2. Actions : Up (state+1) / Down (state-1)
-//3. Reducer
-//4. Dispath =>>> example:
-
-//Init state
-const initState = 0;
-
-//Actions
-const UP_ACTION = "up";
-const DOWN_ACTION = "down";
-
-//Reducer
-const reducer = (state, action) => {
-  console.log("reducer running ...");
-  switch (action) {
-    case UP_ACTION:
-      return state + 1;
-    case DOWN_ACTION:
-      return state - 1;
-    default:
-      throw new Error(`Invalid action `);
-  }
-};
 function App() {
-  const [count, setCount] = useState(0);
-  const [count2, dispatch] = useReducer(reducer, initState);
-
+  const context = useContext(ThemeContext);
   return (
     <div style={{ padding: "0px  20px" }}>
-      <p>Sử dụng useState</p>
-      <h1>{count}</h1>
-      <button onClick={() => setCount(count + 1)}>Up</button>
-      <button onClick={() => setCount(count - 1)}>Down</button>
-      <p>Sử dụng useReducer</p>
-      <h1>{count2}</h1>
-      <button onClick={() => dispatch(UP_ACTION)}>Up</button>
-      <button onClick={() => dispatch(DOWN_ACTION)}>Down</button>
+      <button onClick={context.toggleTheme}>Toggle Theme</button>
+      <Content />
     </div>
   );
 }
